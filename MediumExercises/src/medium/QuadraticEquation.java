e094757e3d66a440cd4b1d784c01c1aadb51bd31
sametcanugur <sametcanugur@localhost>
2024-04-19 15:24:00 +0300

    Program is done.

--- MediumExercises/src/medium/QuadraticEquation.java
+++ MediumExercises/src/medium/QuadraticEquation.java
@@ -4,21 +4,27 @@ public class QuadraticEquation {
 
 	public static void main(String[] args) {
 
-		System.out.println(rootFinder(2, -7, 3));
+		System.out.println(rootFinder(1, -12, -28));
 	}
 
 	public static int rootFinder(double a, double b, double c) {
+
 		double squareRoot = (b * b - (4 * a * c));
 		double calculation = (-b + Math.sqrt(squareRoot)) / (2 * a);
 		String converted = String.valueOf(calculation);
 		String result = "";
+
 		for (int i = 0; i < converted.length(); i++) {
+
 			if (converted.charAt(i) != '.') {
+
 				result = result + converted.charAt(i);
 			} else {
+
 				return Integer.valueOf(result);
 			}
 		}
+
 		return 0;
 	}
 }
