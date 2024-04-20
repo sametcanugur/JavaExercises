0ae90da9b492ff8d859b3b5e5cb6ee9ba6c0752e
sametcanugur <sametcanugur@localhost>
2024-04-20 15:24:37 +0300

    Program is done.

--- MediumExercises/src/medium/TheEmptySquareSequence.java
+++ MediumExercises/src/medium/TheEmptySquareSequence.java
@@ -3,16 +3,18 @@ package medium;
 public class TheEmptySquareSequence {
 
 	public static void main(String[] args) {
-		
+
 		System.out.println(emptySquareCounter(5));
 	}
 
 	public static int emptySquareCounter(int number) {
-		
-		int result=0;
-		for(int i=1; i<=number; i++) {
-			result=result+(2*(i-1))*4;
+
+		int result = 0;
+		for (int i = 1; i <= number; i++) {
+
+			result = result + (2 * (i - 1)) * 4;
 		}
+
 		return result;
 	}
 }
