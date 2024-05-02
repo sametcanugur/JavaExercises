36efe819a0beaf72340befc6858d02ca41c94add
sametcanugur <sametcanugur@localhost>
2024-05-02 10:48:00 +0300

    Program is done.

---
+++ MediumExercises/src/medium/ReversetheCase.java
@@ -0,0 +1,28 @@
+package medium;
+
+public class ReversetheCase {
+
+	public static void main(String[] args) {
+
+		System.out.println(caseReverser("sPoNtAnEoUs"));
+	}
+
+	public static String caseReverser(String text) {
+
+		String[] array = text.split("");
+		String result = "";
+
+		for (int i = 0; i < array.length; i++) {
+
+			if (array[i].equals(array[i].toUpperCase())) {
+
+				result = result + array[i].toLowerCase();
+			} else {
+
+				result = result + array[i].toUpperCase();
+			}
+		}
+
+		return result;
+	}
+}
